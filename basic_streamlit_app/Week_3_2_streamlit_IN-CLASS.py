5528dd9f6796a4b908f6706909578275ffd1e14b
annamccartan3 <annamccartan3@gmail.com>
2025-02-04 10:22:03 -0500

    delete some folders

--- basic_streamlit_app/Week_3_2_streamlit_IN-CLASS.py
+++ basic_streamlit_app/Week_3_2_streamlit_IN-CLASS.py
@@ -3,10 +3,14 @@ import streamlit as st
 
 # Navigate
 # ls
+# cd
 
 # Display a simple text message
+st.write("This is my first streamlit app!")
 
 # Display a large title on the app
+st.title("Hello, streamlit!")
+st.markdown("### This is my first markdown!")
 
 # ------------------------
 # INTERACTIVE BUTTON
@@ -14,7 +18,10 @@ import streamlit as st
 
 # Create a button that users can click.
 # If the button is clicked, the message changes.
-
+if st.button("Click Me"):
+  st.write("You clicked the button! Nice work!")
+else:
+  st.write("Go ahead...click the button. I dare you.")
 # ------------------------
 # COLOR PICKER WIDGET
 # ------------------------
@@ -29,11 +36,15 @@ import streamlit as st
 # ------------------------
 
 # Import pandas for handling tabular data
+import pandas as pd
+import numpy as np
 
 # Display a section title
+st.header("Data Inputs")
 
 # Create a simple Pandas DataFrame with sample data
-
+df = pd.DataFrame(np.random.randn(50, 20), columns=("col %d" % i for i in range(20)))
+st.dataframe(df)  # Same as st.write(df)
 
 # Display a descriptive message
 
@@ -60,6 +71,8 @@ import streamlit as st
 # https://docs.streamlit.io/develop/api-reference
 # Use the cheat sheet for quick reference:
 # https://cheat-sheet.streamlit.app/
+values = st.slider("Select a range of values", 0.0, 100.0, (25.0, 75.0))
+st.write("Values:", values)
 
 ### Challenge:
 # 1️⃣ Modify the dataframe (add new columns or different data).
