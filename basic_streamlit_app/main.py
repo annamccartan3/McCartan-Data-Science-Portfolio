1aae7f298bb6cd1c2052d6ea7f08fa4ae3418eff
annamccartan3 <annamccartan3@gmail.com>
2025-02-09 11:20:53 -0600

    generate file structure with python file and data input

--- basic_streamlit_app/main.py
+++ basic_streamlit_app/main.py
@@ -1,1 +1,23 @@
+import streamlit as st
+import pandas as pd
 
+# app should display:
+    # A title using st.title()
+    # A short description of what the app does.
+    # A sample DataFrame (to be loaded from a CSV, you may use something like Palmer's Penguins here Download Palmer's Penguins here).
+    # Interactive filtering options (e.g., dropdowns, sliders, etc.).
+
+# title using st.title()
+st.title("DogFinder")
+
+# short description of what the app does
+st.write("A Streamlit app for tracking dog breeds in the United States of America.")
+
+# sample DataFrame of dog breed data, loaded from a CSV
+dog_df = pd.read_csv("data/sample_data.csv")
+st.dataframe(dog_df)
+
+# interactive filtering options
+    # dropdowns
+
+    # sliders
