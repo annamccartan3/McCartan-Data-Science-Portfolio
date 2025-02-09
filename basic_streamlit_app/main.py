3fe8e5ef4ef1c9b9e9a6f67aa093c256c2292458
annamccartan3 <annamccartan3@gmail.com>
2025-02-09 11:56:09 -0600

    import dog breed data and add height/weight/breed group filters

--- basic_streamlit_app/main.py
+++ basic_streamlit_app/main.py
@@ -8,16 +8,30 @@ import pandas as pd
     # Interactive filtering options (e.g., dropdowns, sliders, etc.).
 
 # title using st.title()
-st.title("DogFinder")
-
+st.title("DogFinder!")
+st.subheader("A Streamlit app for tracking dog breeds.")
 # short description of what the app does
-st.write("A Streamlit app for tracking dog breeds in the United States of America.")
+st.write("Use the dropdown and sliders to filter by dog breed group and maximum heights and weights.")
 
 # sample DataFrame of dog breed data, loaded from a CSV
-dog_df = pd.read_csv("data/sample_data.csv")
-st.dataframe(dog_df)
+dog_df = pd.read_csv("basic_streamlit_app/data/my_data.csv")
+# drop unnecessary metrics
+dog_df = dog_df.drop(["Detailed Description Link", "Height", "Weight", "Life Span"], axis=1)
 
 # interactive filtering options
-    # dropdowns
-
+    
+    # dropdown
+group = st.selectbox("Select a Dog Breed Group", dog_df["Dog Breed Group"].unique())
     # sliders
+height = st.slider("Choose a maximum height:",
+                    min_value = dog_df["Avg. Height, cm"].min(),
+                    max_value = dog_df["Avg. Height, cm"].max())
+weight = st.slider("Choose a maximum weight:",
+                    min_value = dog_df["Avg. Weight, kg"].min(),
+                    max_value = dog_df["Avg. Weight, kg"].max())
+
+# filter dataframe based on user choices
+st.write(f"{group} under {height} cm and {weight} kg:")
+st.dataframe(dog_df[(dog_df['Dog Breed Group']==group)&
+                    (dog_df['Avg. Height, cm'] <= height)&
+                    (dog_df['Avg. Weight, kg'] <= weight)])
