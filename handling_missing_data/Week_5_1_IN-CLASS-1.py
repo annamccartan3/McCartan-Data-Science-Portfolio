4244162de49d79cecd1412705ee57f05d150920e
annamccartan3 <annamccartan3@gmail.com>
2025-02-13 10:33:27 -0500

    employ data imputation methods and visualize resulting changes

--- handling_missing_data/Week_5_1_IN-CLASS-1.py
+++ handling_missing_data/Week_5_1_IN-CLASS-1.py
@@ -59,16 +59,42 @@ st.pyplot(fig)
 # - Dropping columns if more than 50% of the values are missing
 # - Imputing missing values with mean, median, or zero
 # ================================================================================
-st.subheader("Handling Missing Data")
+st.subheader("Handle Missing Data")
 
 # Work on a copy of the DataFrame so the original data remains unchanged.
 column = st.selectbox("Choose a column to fill", df.select_dtypes(include=["number"]).columns)
 # Apply the selected method to handle missing data.
 st.dataframe(df[column])
 
-st.radio("Choose a method:", 
-         options=["Original DF", "Drop Rows", "Drop Columns", 
-"Impute Mean", "Impute Median", "Impute Zero"])
+method = st.radio("Choose a method:",
+         ["Original DF", "Drop Rows", "Drop Columns (>50% Missing)", 
+          "Impute Mean", "Impute Median", "Impute Zero"])
+
+# Copy our original dataframe
+# df remains untouched
+# df_clean will be the imputation/deletion dataframe
+df_clean = df.copy()
+
+if method == "Original DF":
+    pass
+elif method == "Drop Rows":
+    df_clean = df_clean.dropna(subset = [column])
+elif method == "Drop Columns (>50% Missing)":
+    df_clean = df_clean.drop(columns=df_clean.columns[df_clean.isnull().mean() > 0.5])
+elif method == "Impute Mean":
+    df_clean[column] = df_clean[column].fillna(df[column].mean())
+elif method == "Impute Median":
+    df_clean[column] = df_clean[column].fillna(df[column].median())
+elif method == "Impute Zero":
+    df_clean[column] = df_clean[column].fillna(0)
+
+st.subheader("Cleaned Data Distribution")
+fig, ax = plt.subplots()
+sns.histplot(df_clean[column], kde = True)
+st.pyplot(fig)
+
+#st.dataframe(df_clean)
+#st.write(df_clean.describe())
 
 # ------------------------------------------------------------------------------
 # Compare Data Distributions: Original vs. Cleaned
