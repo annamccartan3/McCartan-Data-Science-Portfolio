f1ba1bb756283d5e1a7d7d91afd2a6ff353614b3
annamccartan <annamccartan3@gmail.com>
2025-05-08 20:49:09 -0400

    change file path

--- MLUnsupervisedApp/main.py
+++ MLUnsupervisedApp/main.py
@@ -111,7 +111,8 @@ def handle_preloaded_data(name):
             'target_names': data.target_names.tolist()
         })
     elif name == "Countries":
-        df = pd.read_csv("data/Country-data.csv")
+        #df = pd.read_csv("data/Country-data.csv")
+        df = pd.read_csv("MLUnsupervisedApp/data/Country-data.csv")
         default_target = "country" if "country" in df.columns else df.columns[0]
         
         st.session_state.update({
